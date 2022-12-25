a16dc8386bf1cb5ed7d31c1011b649497be7d56a
test <gdcool@inbox.ru>
2022-12-25 12:24:44 +0300

    upd1

---
+++ app/src/main/java/com/example/smartfitness/Constance.kt
@@ -0,0 +1,16 @@
+package com.example.smartfitness
+
+object Constance {
+    const val onepush = 10
+    const val onepull = 7
+    const val onesquat = 25
+    const val oneabc = 25
+    const val oneexten = 25
+
+    const val twopush = 25
+    const val twopull = 15
+    const val twosquat = 40
+    const val twoabc = 50
+    const val twoexten = 50
+
+}
